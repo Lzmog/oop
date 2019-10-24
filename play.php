95f85d4c1ccde8ae65531e4c080c478ecda01b63
Lukas Zmoginas <lukas.zmoginas@nfq.lt>
2019-10-24 19:07:03 +0300

    Video 4 | Method | OOP1

--- play.php
+++ play.php
@@ -19,6 +19,28 @@ class Ship
     {
         return $this->name;
     }
+
+    public function getNameAndSpecs($useShortFormat)
+    {
+
+        if ($useShortFormat) {
+            return sprintf(
+                '%s: %s/%s/%s',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        } else {
+            return sprintf(
+                '%s: w:%s, j:%s, s:%s',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        }
+    }
 }
 
 $myship = new Ship();
@@ -30,3 +52,7 @@ echo '<hr/>';
 $myship->sayHello();
 echo '<hr/>';
 echo $myship->getName();
+echo '<hr/>';
+echo $myship->getNameAndSpecs(false);
+echo '<hr/>';
+echo $myship->getNameAndSpecs(true);
