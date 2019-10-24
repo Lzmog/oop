97b8561e5ee3e9c9f568aaa477650de0793b69b7
Lukas Zmoginas <lukas.zmoginas@nfq.lt>
2019-10-24 19:02:05 +0300

    Video 3 | Method | OOP1

--- play.php
+++ play.php
@@ -1,13 +1,32 @@
 <?php
 
-declare(strict_types=1);
-
 class Ship
 {
     public $name;
+
+    public $weaponPower = 0;
+
+    public $jediFactor = 0;
+
+    public $strength = 0;
+
+    public function sayHello()
+    {
+        echo 'HELLO!';
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
 }
 
 $myship = new Ship();
 $myship->name = 'Jedi Starship';
+$myship->weaponPower = 10;
 
 echo 'Ship name: ' . $myship->name;
+echo '<hr/>';
+$myship->sayHello();
+echo '<hr/>';
+echo $myship->getName();
