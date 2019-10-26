573e21cedbb58f89811f661c565f74dcb44acc6b
Lukas Zmoginas <lukas.zmoginas@nfq.lt>
2019-10-26 19:27:51 +0300

    Video 5 | Creating an Abstract Ship | OOP3

---
+++ lib/Model/AbstractShip.php
@@ -0,0 +1,108 @@
+<?php
+
+class AbstractShip
+{
+    private $id;
+
+    private $name;
+
+    private $weaponPower = 0;
+
+    private $strength = 0;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function sayHello()
+    {
+        echo 'HELLO!';
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getNameAndSpecs($useShortFormat = false)
+    {
+
+        if ($useShortFormat) {
+            return sprintf(
+                '%s: %s/%s/%s',
+                $this->name,
+                $this->weaponPower,
+                $this->getJediFactor(),
+                $this->strength
+            );
+        } else {
+            return sprintf(
+                '%s: w:%s, j:%s, s:%s',
+                $this->name,
+                $this->weaponPower,
+                $this->getJediFactor(),
+                $this->strength
+            );
+        }
+    }
+
+    public function doesGivenShipHaveMoreStrength($givenShip)
+    {
+        return $givenShip->strength > $this->strength;
+    }
+
+    public function setStrength($strength)
+    {
+        if (false === is_numeric($strength)) {
+            throw new Exception('Invalid strength passed');
+        }
+
+        $this->strength = $strength;
+    }
+
+    public function getStrength()
+    {
+        return $this->strength;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWeaponPower()
+    {
+        return $this->weaponPower;
+    }
+
+    /**
+     * @param int $weaponPower
+     */
+    public function setWeaponPower($weaponPower)
+    {
+        $this->weaponPower = $weaponPower;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param integer $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+}
--- lib/Model/RebelShip.php
+++ lib/Model/RebelShip.php
@@ -1,6 +1,6 @@
 <?php
 
-class RebelShip extends Ship
+class RebelShip extends AbstractShip
 {
     public function getFavoriteJedi()
     {
@@ -30,5 +30,10 @@ class RebelShip extends Ship
         $val .= ' (Rebel)';
 
         return $val;
+    }
+
+    public function getJediFactor()
+    {
+        return rand(10, 30);
     }
 }
