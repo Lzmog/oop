b20f928f59c8fa9c7bbe0088a5f85c1b6e304a14
Lukas Zmoginas <lukas.zmoginas@nfq.lt>
2019-10-27 13:38:10 +0200

    Video 6 | More Fun with use Statements | OOP4

--- bootstrap.php
+++ bootstrap.php
@@ -1,15 +1,5 @@
 <?php
 ini_set('display_errors', 1);
-require_once __DIR__ . '/lib/Model/AbstractShip.php';
-require_once __DIR__ . '/lib/Model/Ship.php';
-require_once __DIR__ . '/lib/Model/RebelShip.php';
-require_once __DIR__ . '/lib/Model/BrokenShip.php';
-require_once __DIR__ . '/lib/Service/ShipStorageInterface.php';
-require_once __DIR__ . '/lib/Service/PDOShipStorage.php';
-require_once __DIR__ . '/lib/Service/JsonFileShipStorage.php';
-require_once __DIR__ . '/lib/Service/ShipLoader.php';
-require_once __DIR__ . '/lib/Model/BattleResult.php';
-require_once __DIR__ . '/lib/Service/Container.php';
 
 spl_autoload_register(function ($className) {
     $path = __DIR__ . '/lib/' . str_replace('\\' , '/', $className) . '.php';
